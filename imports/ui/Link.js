1222b948973611af57066da6f8f1fa883e48c7fb
Ricardo Barantini <ricardobarantini@gmail.com>
2018-02-01 10:02:44 -0200

    Section 7 completed

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
+import { Links } from '../api/links';
+
+import LinksList from './LinksList';
 
 export default class Link extends React.Component {
   onLogout() {
     Accounts.logout();
+  }
+
+  onSubmit(e) {
+    const url = this.refs.url.value.trim();
+
+    e.preventDefault();
+
+    if (url) {
+      // Links.insert({ url, userId: Meteor.userId() });
+      Meteor.call('links.insert', url);
+      this.refs.url.value = '';
+    }
   }
 
   render() {
@@ -11,6 +27,12 @@ export default class Link extends React.Component {
       <div>
         <h1>Links</h1>
         <button onClick={this.onLogout.bind(this)}>Logout</button>
+        <LinksList/>
+        <p>Add Link</p>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="text" ref="url" placeholder="URL"/>
+          <button>Add Link</button>
+        </form>
       </div>
     );
   }
--- server/main.js
+++ server/main.js
@@ -1,7 +1,36 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users';
+import { Links } from '../imports/api/links';
+import '../imports/startup/simple-schema-configuration';
 
 Meteor.startup(() => {
-  //
+  WebApp.connectHandlers.use((req, res, next) => {
+    console.log('This is from my custom middleware');
+    console.log(req.url, req.method, req.headers, req.query);
+    // Set HTTP status code
+    // res.statusCode = 404;
+    // Set HTTP headers
+    // res.setHeader('my-custom-header', 'Ricardo');
+    // Set HTTP body
+    // res.write('<h1>This is my middleware at work!</h1>');
+    // End HTTP request
+    // res.end();
+
+    next();
+  });
+
+  WebApp.connectHandlers.use((req, res, next) => {
+    const _id = req.url.slice(1);
+    const link = Links.findOne({ _id });
+
+    if (link) {
+      res.statusCode = 302;
+      res.setHeader('Location', link.url);
+      res.end();
+    } else {
+      next();
+    }
+  });
 });
