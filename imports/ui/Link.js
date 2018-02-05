cb5bb280fe31e5b8494c3b58e3a0e1ea7aa6b180
Ricardo Barantini <ricardobarantini@gmail.com>
2018-02-05 09:20:20 -0200

    Work in Progress

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -4,36 +4,15 @@ import { Accounts } from 'meteor/accounts-base';
 import { Links } from '../api/links';
 
 import LinksList from './LinksList';
+import PrivateHeader from './PrivateHeader';
+import AddLink from './AddLink';
 
-export default class Link extends React.Component {
-  onLogout() {
-    Accounts.logout();
-  }
-
-  onSubmit(e) {
-    const url = this.refs.url.value.trim();
-
-    e.preventDefault();
-
-    if (url) {
-      // Links.insert({ url, userId: Meteor.userId() });
-      Meteor.call('links.insert', url);
-      this.refs.url.value = '';
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Links</h1>
-        <button onClick={this.onLogout.bind(this)}>Logout</button>
-        <LinksList/>
-        <p>Add Link</p>
-        <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="URL"/>
-          <button>Add Link</button>
-        </form>
-      </div>
-    );
-  }
-}
+export default () => {
+  return (
+    <div>
+      <PrivateHeader title="Your Links"/>
+      <LinksList/>
+      <AddLink/>
+    </div>
+  );
+};
---
+++ imports/ui/LinksListItem.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Clipboard from 'clipboard';
+
+export default class LinksListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      justCopied: false
+    };
+  }
+
+  componentDidMount() {
+    this.clipboard = new Clipboard(this.refs.copy);
+
+    this.clipboard.on('success', () => {
+      this.setState({justCopied: true});
+
+      setTimeout(() => {
+        this.setState({justCopied: false});
+      }, 1000);
+    }).on('error', () => {
+      alert('Error');
+    });
+  }
+
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.props.url}</p>
+        <p>{this.props.shortUrl}</p>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>{this.state.justCopied ? 'Copied' : 'Copy'}</button>
+      </div>
+    );
+  }
+};
+
+LinksListItem.propTypes = {
+  _id: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+  userId: PropTypes.string.isRequired,
+  shortUrl: PropTypes.string.isRequired
+};
